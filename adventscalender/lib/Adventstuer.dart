933110d0299eb73ce17e8574cfd952c0e610edd0
paradoxon <blablabla!1865Atlassian>
2022-11-28 21:35:04 +0100

    Finally fixed the Layout of Back of the advents door :)

--- adventscalender/lib/Adventstuer.dart
+++ adventscalender/lib/Adventstuer.dart
@@ -3,7 +3,6 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flip_card/flip_card.dart';
 
-
 class Adventstuer extends StatelessWidget {
   Adventstuer({required this.Tag, required this.backModel});
   final Tag;
@@ -11,82 +10,81 @@ class Adventstuer extends StatelessWidget {
   Widget build(BuildContext context) {
     DateTime date = DateTime.now();
     return Card(
-      elevation: 0.0,
-      margin: EdgeInsets.only(left: 32.0, right: 32.0, top: 32.0, bottom: 32.0),
-      color: Color(0x00000000),
-      child: FlipCard(
-        flipOnTouch: Tag<date.day, //&& date.month==12,
-        direction: FlipDirection.HORIZONTAL,
-        speed: 1000,
-        onFlipDone: (status) {
-          print(status);
-        },
-        front: Container(
-          decoration: BoxDecoration(
-            color: Color(0xFF993333),
-            borderRadius: BorderRadius.all(Radius.circular(8.0)),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(Tag.toString(),
-                  style: Theme.of(context).textTheme.headline1!.apply(fontFamily: 'Cartoon Blocks Christmas')
+        elevation: 0.0,
+        margin:
+            EdgeInsets.only(left: 2.0, right: 2.0, top: 2.0, bottom: 2.0),
+        color: Color(0x00000000),
+        child: FlipCard(
+            flipOnTouch: Tag < date.day, //&& date.month==12,
+            direction: FlipDirection.HORIZONTAL,
+            speed: 1000,
+            onFlipDone: (status) {
+              //later store the stat of this widget here
+              print(status);
+            },
+            front: Container(
+              decoration: BoxDecoration(
+                color: Color(0xFF993333),
+                borderRadius: BorderRadius.all(Radius.circular(8.0)),
               ),
-            ],
-          ),
-        ),
-        back: Container(
-          decoration: BoxDecoration(
-            color: Color(0xFF007777),
-            borderRadius: BorderRadius.all(Radius.circular(8.0)),
-          ),
-          /*child: Center(
-            child:
-              Padding(
-                padding: EdgeInsets.all(15),
-                  child: AutoSizeText(Inhalt,
-                    style: Theme.of(context).textTheme.headline1
-                  )
-              )
-          ),*/
-          child:
-        Padding(
-        padding: EdgeInsets.all(15),
-          child:  Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                //Header Container
-                Container(
-                  padding: const EdgeInsets.all(2.0),
-                  alignment: Alignment.center,
-                  child: Text(backModel.head,
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  AutoSizeText(Tag.toString(),
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline1!
+                          .apply(fontFamily: 'Cartoon Blocks Christmas'))
+                ],
+              ),
+            ),
+            back: Container(
+                decoration: BoxDecoration(
+                  color: Color(0xFF007777),
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
                 ),
-                //Body Container
-                Expanded(
-                      child: AutoSizeText(backModel.main,
-                              style: Theme.of(context).textTheme.headline1,
-                              wrapWords: false,
-                              softWrap: true,
+                child: Padding(
+                    padding: EdgeInsets.all(2),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          //Header Container
+                            Padding(
+                              padding: EdgeInsets.all(2),
+                              child: AutoSizeText(
+                                  maxLines: 1,
+                                  wrapWords: false,
+                                  softWrap: false,
+                                  backModel.head,
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
                             ),
-                  ),
-
-                //Footer Container
-                //Here you will get unexpected behaviour when keyboard pops-up.
-                //So its better to use `bottomNavigationBar` to avoid this.
-                Container(
-                  padding: const EdgeInsets.all(2.0),
-                  alignment: Alignment.centerRight,
-                  child: Text(backModel.footer,
-                    style: TextStyle(fontStyle: FontStyle.italic),
-                  ),
-                )
-              ]
-          )
-        )
-        )
-      )
-    );
+                          //Body Container
+                          Expanded(
+                            child: Padding(
+                              padding: EdgeInsets.all(2),
+                              child: AutoSizeText(
+                                backModel.main,
+                                style: Theme.of(context).textTheme.headline1,
+                                softWrap: true,
+                              ),
+                            ),
+                          ),
+                          //Footer Container
+                          //Here you will get unexpected behaviour when keyboard pops-up.
+                          //So its better to use `bottomNavigationBar` to avoid this.
+                          Padding(
+                            padding: EdgeInsets.all(2),
+                            child: AutoSizeText(
+                                  backModel.footer,
+                                  maxLines: 1,
+                                  wrapWords: false,
+                                  softWrap: false,
+                                  minFontSize: 2.0,
+                                  textAlign: TextAlign.right,
+                                  style: TextStyle(fontStyle: FontStyle.italic),
+                                ),
+                          ),
+                        ])))));
   }
 }

e71364b3267447e4f733d0109467502eed396774
paradoxon <blablabla!1865Atlassian>
2022-12-01 09:37:12 +0100

    Adventskalender input in Dateien ausgelagert, um einfach schnell Inhalte ändern zu können.

---
+++ adventscalender/lib/heilsarmee.dart
@@ -0,0 +1,29 @@
+import 'models/back_model.dart';
+final String title = "Adventskalender für Dich";
+
+final Map<int,BackModel> tuer ={
+  1: BackModel(head:"",main: "Advent und Weihnachten ist wie ein Schlüsselloch, durch das auf unsren dunklen Erdenweg ein Schein aus der Heimat fällt.",footer: "Friedrich von Bodelschwingh"),
+  2: BackModel(head:"",main: "Erlebt es selbst, wie gut Gott ist! Glücklich ist derjenige, der bei ihm Zuflucht sucht.",footer: ""),
+  3: BackModel(head:"",main: "Die Tage sind so dunkel, die Nächte lang und kalt; doch übet Sterngefunkel noch über uns Gewalt. Wir wollen nach dir blicken, du Licht das ewig brennt. Wir wollen uns beschicken zum seligen Advent.",footer: "Max von Schenkendorf"),
+  4: BackModel(head:"",main: "Gott erfüllt mein Herz mit großer Freude, er richtet mich auf und gibt mir neue Kraft.",footer: "1. Samuel 2, 1"),
+  5: BackModel(head:"",main: "Advent ist die Zeit der Besinnung. Man hetzt von Besinnungsstunde zu Besinnungsstunde.",footer: "Gabriella Leone-Eckhardt"),
+  6: BackModel(head:"",main: "Gott ist Liebe und wer in dieser Liebe bleibt, der bleibt in Gott und Gott in ihm.",footer: "1. Johannes 4, 16"),
+  7: BackModel(head:"",main: "",footer: ""),
+  8: BackModel(head:"",main: "",footer: ""),
+  9: BackModel(head:"",main: "",footer: ""),
+  10: BackModel(head:"",main: "",footer: ""),
+  11: BackModel(head:"",main: "",footer: ""),
+  12: BackModel(head:"",main: "",footer: ""),
+  13: BackModel(head:"",main: "",footer: ""),
+  14: BackModel(head:"",main: "",footer: ""),
+  15: BackModel(head:"",main: "",footer: ""),
+  16: BackModel(head:"",main: "",footer: ""),
+  17: BackModel(head:"",main: "",footer: ""),
+  18: BackModel(head:"",main: "",footer: ""),
+  19: BackModel(head:"",main: "",footer: ""),
+  20: BackModel(head:"",main: "",footer: ""),
+  21: BackModel(head:"",main: "",footer: ""),
+  22: BackModel(head:"",main: "",footer: ""),
+  23: BackModel(head:"",main: "",footer: ""),
+  24: BackModel(head:"",main: "",footer: "")
+};
--- adventscalender/lib/main.dart
+++ adventscalender/lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:adventscalender/models/back_model.dart';
 import 'package:flutter/material.dart';
 
 import 'Adventstuer.dart';
+import 'heilsarmee.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: 'Adventskalender für Anni'),
+      home: MyHomePage(title: title),
     );
   }
 }
@@ -34,32 +34,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   MyHomePage({super.key, required this.title});
   final String title;
-  final Map<int,BackModel> tuer ={
-     1: BackModel(head:"Was ich an dir liebe",main: "Dein Durchhaltevermögen",footer: "dein stolzer Matthias"),
-     2: BackModel(head:"Was Gott über dich denkt",main: "Du bist wunderbar gemacht.",footer: "Psalm 139,14"),
-     3: BackModel(head:"Was ich an dir bewundere",main: "Deine Disziplin",footer: "Matthias wars"),
-     4: BackModel(head:"Wie krass wertvoll du für mich und IHN bist",main: "Du hast ihn nur wenig geringer gemacht als die Engel , ja, mit Ruhm und Ehre hast du ihn gekrönt.",footer: "Psalm 8:6"),
-     5: BackModel(head:"Was mich an dir beeindruckt",main: "Dein Stringenz, Dein unglaubliches literatisches Talent und so viel mehr",footer: "schon wieder Matthias"),
-     6: BackModel(head:"Er und ich freuen sich über Dich ",main: "Der Herr , euer Gott, ist in eurer Mitte; und was für ein starker Retter ist er! Von ganzem Herzen freut er sich über euch. Weil er euch liebt, redet er nicht länger über eure Schuld. Ja, er jubelt, wenn er an euch denkt!«",footer: "Zefanja 3:17, HFA"),
-     7: BackModel(head:"Ich freu mich über Deine",main: "Kreativität",footer: "WOW"),
-     8: BackModel(head:"Er sorgt sich um Dich",main: "Macht euch also keine Sorgen! Fragt nicht: Was sollen wir essen? Was sollen wir trinken? Was sollen wir anziehen? Denn um diese Dinge geht es den Heiden, die Gott nicht kennen. Euer Vater im Himmel aber weiß, dass ihr das alles braucht.",footer: "(Matthäus 6, 31-32 NGÜ)"),
-     9: BackModel(head:"Was ich an dir liebe",main: "Deine Einfühlungsvermögen",footer: "Dein CoChef und Ehemann"),
-    10: BackModel(head:"Gott zu dir und du bist so",main: "Wie einen, den seine Mutter tröstet, so will ich euch trösten; ",footer: "Jesaja 66, 13"),
-    11: BackModel(head:"Was ich an dir liebe",main: "Dein Mutterinstinkt",footer: "Matthias"),
-    12: BackModel(head:"Zuerst er, aber auch ich",main: "Mit ewiger Liebe habe ich dich geliebt; darum habe ich dich zu mir gezogen aus lauter Gnade.",footer: "Jermia 31, 3"),
-    13: BackModel(head:"Was ich an dir liebe",main: "Dein Dranbleiben",footer: "Dein Ehemann"),
-    14: BackModel(head:"Lass uns durchhalten",main: "Ihm, der mit seiner unerschöpflichen Kraft in uns[25] am Werk ist und unendlich viel mehr zu tun vermag, als wir erbitten oder begreifen können",footer: "Epheser 3, 20"),
-    15: BackModel(head:"Du beeindruckst mich",main: "Mit deinem Fleiß",footer: "Paradoxons wars"),
-    16: BackModel(head:"Sein Versprechen",main: "Der HERR ist nahe denen, die zerbrochenen Herzens sind, und er hilft denen, die zerschlagenen Geistes sind.",footer: "Psalm 34"),
-    17: BackModel(head:"Was mich an dir verzaubert",main: "Deine Musikalität (gaaaanz besonders deine Stimme WOW)",footer: "Dein Lobpreispartner"),
-    18: BackModel(head:"Mein Gebet und er wird es machen",main: "Wir bitten unseren Herrn Jesus Christus und Gott, unseren Vater, der uns seine Liebe erwiesen und uns in seiner Gnade eine nie versiegende Ermutigung und eine sichere Hoffnung geschenkt hat, euch in eurem Innersten zu ermutigen und euch die nötige Kraft für jede gute Tat und jedes gute Wort zu geben.",footer: "2.Thessalonicher 2,16-17"),
-    19: BackModel(head:"Was mich zum lachen bringt",main: "Dein Humor",footer: "Wer wohl :-)"),
-    20: BackModel(head:"Gott spricht dir zu",main: "Der Gerechte muss viel Böses erleiden; aber aus allem rettet ihn der HERR",footer: "Psalm 34, 20"),
-    21: BackModel(head:"Was zusammen mit dir trägt",main: "Deine Jesusliebe",footer: "Dein Freund"),
-    22: BackModel(head:"Das macht Gott für Dich",main: "Gepriesen sei Gott, der Vater unseres Herrn Jesus Christus! Denn er ist ein Vater, der sich erbarmt, und ein Gott, der auf jede erdenkliche Weise tröstet und ermutigt.",footer: "2. Korinther 1, 3"),
-    23: BackModel(head:"Was ich an dir liebe",main: "Dein Durcharbeiten von Herausforderungen",footer: "Matthias war"),
-    24: BackModel(head:"❤❤❤❤❤❤❤",main: "Seht doch, wie groß die Liebe ist, die uns der Vater erwiesen hat: Kinder Gottes dürfen wir uns nennen, und wir sind es tatsächlich!",footer: "1. Johannes 3, 1"),
-  };
+
 
 
   @override
@@ -79,10 +54,11 @@ class MyHomePage extends StatelessWidget {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
+
         child: GridView.count(
           // Create a grid with 2 columns. If you change the scrollDirection to
           // horizontal, this produces 2 rows.
-          crossAxisCount: 2,
+          crossAxisCount: 4,
           mainAxisSpacing: 1.0,
           // Generate 100 widgets that display their index in the List.
           children:
